c4a4497c4dba95e9a4ec2b0803bc6db9592e46bf
Matthew Chandler <mcaz689@gmail.com>
2019-12-10 21:40:56 -0700

    export a function, idiota

--- functions/Authorization.js
+++ functions/Authorization.js
@@ -1,6 +1,6 @@
-exports.Authorization = (req, res) => {
+module.exports = function Authorization(req, res) {
   const scopes = 'user-read-private user-read-email';
   const { client_id, redirect_uri } = process.env;
 
   res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`);
-};
+}
