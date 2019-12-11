f8dd6f1d6118859d1249b43e67588de56158b727
Matthew Chandler <mcaz689@gmail.com>
2019-12-10 23:54:50 -0700

    y

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const request = require('request');
 module.exports = {
   Authorization(req, res) {
     const { client_id, redirect_uri, client_secret } = process.env;
+    const { code } = req.query;
 
-    if (!req.query.code) {
+    if (!code) {
       return res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent('user-read-private user-read-email')}&redirect_uri=${encodeURIComponent(redirect_uri)}`);
     }
 
     const postBody = {
-      grant_type: 'authorization_code',
-      code: req.query.code,
-      redirect_uri: process.env.redirect_uri,
       client_id,
+      redirect_uri,
       client_secret,
+      code: code,
+      grant_type: 'authorization_code',
     };
     console.log(postBody)
 
