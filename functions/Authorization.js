f9e80e3fd0e512562c56ff526c2a6e7b010288d0
Matthew Chandler <mcaz689@gmail.com>
2019-12-10 20:56:42 -0700

    update Authorization endpoint to reference env vars

--- functions/Authorization.js
+++ functions/Authorization.js
@@ -1,6 +1,6 @@
-
-
 exports.authorize = (req, res) => {
   const scopes = 'user-read-private user-read-email';
-  res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${my_client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=encodeURIComponent(redirect_uri)`);
+  const { client_id, redirect_uri } = process.env;
+
+  res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`);
 };
