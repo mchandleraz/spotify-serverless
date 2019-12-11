53076e50b0d1f71a36d8e292014888fd1128c2a8
Matthew Chandler <mcaz689@gmail.com>
2019-12-10 22:47:45 -0700

    export an object

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-module.exports = function Authorization(req, res) {
-  if (!req.query.code) {
-    const scopes = 'user-read-private user-read-email';
-    const { client_id, redirect_uri } = process.env;
+module.exports = {
+  Authorization(req, res) {
+    if (!req.query.code) {
+      const scopes = 'user-read-private user-read-email';
+      const { client_id, redirect_uri } = process.env;
 
-    return res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`);
-  }
-
-  request.post({
-    url: 'https://accounts.spotify.com/api/token',
-    body: {
-      grant_type: 'authorization_code',
-      code: req.query.code,
-      redirect_uri: process.env.redirect_uri,
+      return res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`);
     }
-  }, (err, response, body) => {
-    res.send(body)
-  });
-};
+
+    request.post({
+      url: 'https://accounts.spotify.com/api/token',
+      body: {
+        grant_type: 'authorization_code',
+        code: req.query.code,
+        redirect_uri: process.env.redirect_uri,
+      }
+    }, (err, response, body) => {
+      res.send(body)
+    });
+  }
+}
