be711865dfea9b06502d858b3293dfd63e62e4ac
Matthew Chandler <mcaz689@gmail.com>
2019-12-10 23:18:35 -0700

    yo

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ module.exports = {
       return res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`);
     }
 
+    const postBody = {
+      grant_type: 'authorization_code',
+      code: req.query.code,
+      redirect_uri: process.env.redirect_uri,
+    };
+    console.log(postBody)
+
     request.post({
       url: 'https://accounts.spotify.com/api/token',
-      body: JSON.stringify({
-        grant_type: 'authorization_code',
-        code: req.query.code,
-        redirect_uri: process.env.redirect_uri,
-      }),
+      body: JSON.stringify(postBody),
       json: true,
     }, (err, response, body) => {
       if (err) {
-        console.log(err)
+        console.log('err', err)
         return res.sendStatus(500)
       }
 
-      console.log(body)
+      console.log('body', body)
 
       res.sendStatus(200);
     });
