22cc2432845867aea02ba04e1d23a517f8f71656
Matthew Chandler <mcaz689@gmail.com>
2019-12-10 21:42:42 -0700

    debug missing env vars

--- functions/Authorization.js
+++ functions/Authorization.js
@@ -1,5 +1,6 @@
 module.exports = function Authorization(req, res) {
   const scopes = 'user-read-private user-read-email';
+  console.log(process.env, Object.keys(process))
   const { client_id, redirect_uri } = process.env;
 
   res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`);
