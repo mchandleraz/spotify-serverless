565097180decaee26e590d457754d9845fef3ab2
Matthew Chandler <mcaz689@gmail.com>
2019-12-10 20:14:37 -0700

    Authorization function

---
+++ functions/Authorization.js
@@ -0,0 +1,6 @@
+
+
+exports.authorize = (req, res) => {
+  const scopes = 'user-read-private user-read-email';
+  res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${my_client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=encodeURIComponent(redirect_uri)`);
+};
