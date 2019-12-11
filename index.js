b7bae197341a80a361693485ea0885b7adf290f9
Matthew Chandler <mcaz689@gmail.com>
2019-12-10 23:29:49 -0700

    add client secret

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   Authorization(req, res) {
     if (!req.query.code) {
       const scopes = 'user-read-private user-read-email';
-      const { client_id, redirect_uri } = process.env;
+      const { client_id, redirect_uri, client_secret } = process.env;
 
       return res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`);
     }
@@ -13,6 +13,8 @@ module.exports = {
       grant_type: 'authorization_code',
       code: req.query.code,
       redirect_uri: process.env.redirect_uri,
+      client_id,
+      client_secret,
     };
     console.log(postBody)
 
